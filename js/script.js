4fb89da493f1968f9092d8bcc9a71efe5eb10dcf
Toke34 <gabrielfurlan50@hotmail.com>
2024-05-07 21:57:07 -0300

    json 2.0

--- js/script.js
+++ js/script.js
@@ -1,37 +1,45 @@
-/*Buscador
-document.addEventListener("DOMContentLoaded", function () {
-    var input = document.getElementById("searchInput");
-    var list = document.getElementById("resultList");
-    var items = list.getElementsByTagName("li");
-    input.addEventListener("input", function () {
-        var searchTerm = input.value.toLowerCase();
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            var text = item.textContent.toLowerCase();
-            if (text.includes(searchTerm)) {
-                item.style.display = "block";
-            } else {
-                item.style.display = "none";
-            }
-        }
-    });
-});
-*/
-fetch(`productos.json`) 
+/*
+fetch("../productos.json") 
     .then(response => response.json()) 
     .then(data => {console.log(data)
-        let cad=`
-        <li>
-        <img class="imagen" src="${data.imagen}">
-        <p class="producto-descripcion">${data.nombre}</p>
-        <p class="producto-precio">$ ${data.precio}</p>
-        </li>
-        
-        `
-        
-
-        document.querySelector("#productos").innerHTML=cad
+        for (let producto of data) {
+            let cade = cade + `
+            <li>
+                <img class="imagen" src="${producto.imagen}">
+                <p class="producto-descripcion">${producto.nombre}</p>
+                <p class="producto-precio">$ ${producto.precio}</p>
+            </li>
+            `
+            document.querySelector("#productos").innerHTML=cade
+       
+        }  
     }
 
 );
 console.log("hola")
+
+*/
+fetch("./js/productos.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        let cade = ''; // Inicializa la variable fuera del bucle
+        for (let producto of data.productos) {
+            cade += `
+            <li>
+                <img class="imagen" src="${producto.imagen}">
+                <p class="producto-descripcion">${producto.nombre}</p>
+                <p class="producto-precio">$ ${producto.precio}</p>
+            </li>
+            `;
+        }
+        document.querySelector("#productos").innerHTML = cade;
+    })
+    .catch(error => {
+        console.error('Error de fetch:', error);
+    });
