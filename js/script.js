41e93e50a778637d798489c38fdd81a6c26e243e
Toke34 <gabrielfurlan50@hotmail.com>
2024-05-16 22:31:53 -0300

    guardar tipo producto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 
-function mostrar(){
+
+// Este codigo es para que me guarde en sessionstorage la categoria y genero.
+
+const enlaces = document.querySelectorAll('.store');
+
+// Itera sobre cada enlace y agrega un listener de evento a cada uno
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function(event) {
+        // Previene el comportamiento predeterminado del enlace
+        event.preventDefault();
+
+        // Obtiene el valor del atributo data-genero
+        const genero = this.getAttribute('data-genero');
+        const categoria = this.getAttribute('data-categoria');
+        
+        // Guarda el valor del atributo data-genero en sessionStorage
+        sessionStorage.setItem('genero', genero);
+        sessionStorage.setItem('categoria', categoria);
+
+        // Redirige a la página especificada en el enlace
+        window.location.href = this.href;
+        
+        console.log("Género:",sessionStorage.getItem('genero')); // Salida: Género: hombre
+        console.log("Categoría:", sessionStorage.getItem('categoria')); // Salida: Categoría: pantalones
+    });
+});
+
+
+/*
+function mostrar(categoria , genero , precio){
 fetch("./js/productos.json")
     .then(response => {
         if (!response.ok) {
@@ -26,4 +55,4 @@ fetch("./js/productos.json")
         console.error('Error de fetch:', error);
     });
 }
-  
+*/
--- js/shop.js
+++ js/shop.js
@@ -1,67 +1,65 @@
-// carrito.js
-document.addEventListener('DOMContentLoaded', () => {
-  const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-  actualizarCarrito(carrito);
-});
 
-function actualizarCarrito(carrito) {
-  const carritoBody = document.querySelector("#carrito-body");
-  const footer = document.querySelector("#footer");
+  document.addEventListener('DOMContentLoaded', () => {
+      const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+      actualizarCarrito(carrito);
+  });
 
-  carritoBody.innerHTML = '';
+  function actualizarCarrito(carrito) {
+      const carritoBody = document.querySelector("#carrito-body");
+      const footer = document.querySelector("#tfooter");
 
-  if (carrito.length === 0) {
-      footer.innerHTML = '<tr><td class="border-bottom-left border-bottom-right" colspan="4">¡No hay ningún elemento en el carrito!</td></tr>';
-      return;
-  }
+      carritoBody.innerHTML = '';
 
-  let totalCarrito = 0;
+      if (carrito.length === 0) {
+          footer.innerHTML = '<tr><td class="border-bottom-left border-bottom-right" colspan="4">¡No hay ningún elemento en el carrito!</td></tr>';
+          return;
+      }
 
-  carrito.forEach(item => {
-      const precioTotal = item.precio * item.cantidad;
-      totalCarrito += precioTotal;
+      let totalCarrito = 0;
 
-      carritoBody.innerHTML += `
+      carrito.forEach(item => {
+          const precioTotal = item.precio * item.cantidad;
+          totalCarrito += precioTotal;
+
+          carritoBody.innerHTML += `
+          <tr>
+              <td>${item.nombre}</td>
+              <td>
+                  <button class="cantidad-btn" data-id="${item.id}" data-action="decrement">-</button>
+                  ${item.cantidad}
+                  <button class="cantidad-btn" data-id="${item.id}" data-action="increment">+</button>
+              </td>
+              <td>$${item.precio.toFixed(2)}</td>
+              <td>$${precioTotal.toFixed(2)}</td>
+          </tr>
+          `;
+      });
+
+      footer.innerHTML = `
       <tr>
-          <td>${item.nombre}</td>
-          <td>
-              <button class="cantidad-btn" data-id="${item.id}" data-action="decrement">-</button>
-              ${item.cantidad}
-              <button class="cantidad-btn" data-id="${item.id}" data-action="increment">+</button>
-          </td>
-          <td>$${item.precio.toFixed(2)}</td>
-          <td>$${precioTotal.toFixed(2)}</td>
+          <td class="border-bottom-left" colspan="3">Total</td>
+          <td class="border-bottom-right">$${totalCarrito.toFixed(2)}</td>
       </tr>
       `;
-  });
 
-  footer.innerHTML = `
-  <tr>
-      <td class="border-bottom-left" colspan="3">Total</td>
-      <td class="border-bottom-right">$${totalCarrito.toFixed(2)}</td>
-  </tr>
-  `;
-
-  document.querySelectorAll('.cantidad-btn').forEach(button => {
-      button.addEventListener('click', event => {
-          const productId = button.getAttribute('data-id');
-          const action = button.getAttribute('data-action');
-          actualizarCantidad(carrito, productId, action);
+      document.querySelectorAll('.cantidad-btn').forEach(button => {
+          button.addEventListener('click', event => {
+              const productId = button.getAttribute('data-id');
+              const action = button.getAttribute('data-action');
+              actualizarCantidad(carrito, productId, action);
+          });
       });
-  });
-}
-
-function actualizarCantidad(carrito, productId, action) {
-  const index = carrito.findIndex(p => p.id == productId);
-  if (index !== -1) {
-      if (action === 'increment') {
-          carrito[index].cantidad += 1;
-      } else if (action === 'decrement' && carrito[index].cantidad > 1) {
-          carrito[index].cantidad -= 1;
-      }
-      localStorage.setItem('carrito', JSON.stringify(carrito));
-      actualizarCarrito(carrito);
   }
-}
 
-
+  function actualizarCantidad(carrito, productId, action) {
+      const index = carrito.findIndex(p => p.id == productId);
+      if (index !== -1) {
+          if (action === 'increment') {
+              carrito[index].cantidad += 1;
+          } else if (action === 'decrement' && carrito[index].cantidad > 1) {
+              carrito[index].cantidad -= 1;
+          }
+          localStorage.setItem('carrito', JSON.stringify(carrito));
+          actualizarCarrito(carrito);
+      }
+  }
