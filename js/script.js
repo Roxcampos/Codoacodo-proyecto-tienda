d0f1c62c509fc17af552fa9abde1e1f294bcfc78
Gabriel <gabrielfurlan50@hotmail.com>
2024-07-08 12:00:48 -0300

    gestion producto

--- js/script.js
+++ js/script.js
@@ -167,3 +167,22 @@ function logueado(){
     }
 });
 }
+
+function openTab(tabName) {
+    var i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.querySelector(button[onclick="openTab('${tabName}')"]).className += " active";
+}
+
+// Inicializa la primera pestaña activa
+document.addEventListener("DOMContentLoaded", function() {
+    openTab('crear');
+});
